533d90d672896acf4e92afbc67bfd29d5740d260
f62esreb <f62esreb@uco.es>
2024-02-21 10:48:01 +0100

    Añadida p0

---
+++ vector2d/vector2d_imp.hpp
@@ -0,0 +1,16 @@
+/**
+ * CopyRight F. J. Madrid-Cuevas <fjmadrid@uco.es>
+ *
+ * Sólo se permite el uso de este código en la docencia de las asignaturas sobre
+ * Estructuras de Datos de la Universidad de Córdoba.
+ *
+ * Está prohibido su uso para cualquier otro objetivo.
+ */
+#pragma once
+#include <cassert>
+
+#include "vector2d.hpp"
+
+// TODO: implement the interface for the proposed ADT.
+
+//
